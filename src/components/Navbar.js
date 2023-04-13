9cca623a37298b22432d36effaf01077e1efad89
Ale Kennedy <alepertuz@gmail.com>
2023-04-13 14:58:13 -0400

    navbar and header ready

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import '../Navbar.css';
 
 function Navbar() {
   return (
-    <nav className='navbar'>
-      <div className='navbar-left'>
-        <Link className='navbar-link' to='about-section' smooth={true} duration={500}>
-          About
-        </Link>
-        <Link className='navbar-link' to='projects-section' smooth={true} duration={500}>
-          Projects
-        </Link>
-        <Link className='navbar-link' to='contact-section' smooth={true} duration={500}>
-          Contact
-        </Link>
-      </div>
-      <div className='navbar-right'>
-        <Link className='navbar-logo-link' to='/'>
-          <img className='navbar-logo' src='../../public/images/aklogo.png' alt='Logo' />
-        </Link>
-      </div>
-    </nav>
+   <nav className='flex flex-col md:flex-row items-center justify-between h-1/5 font-mavenpro'>
+   <div className='flex items-center order-1 p-5'>
+     <Link className='mr-6 text-lg text-gray-700 hover:text-indigo-600' to='/' smooth={true} duration={500}>
+       Home
+     </Link>
+     <Link className='mr-6 text-lg text-gray-700 hover:text-indigo-600' to='about-section' smooth={true} duration={500}>
+       About
+     </Link>
+     <Link className='mr-6 text-lg text-gray-700 hover:text-indigo-600' to='projects-section' smooth={true} duration={500}>
+       Projects
+     </Link>
+     <Link className='text-lg text-gray-700 hover:text-indigo-600' to='contact-section' smooth={true} duration={500}>
+       Contact
+     </Link>
+   </div>
+   <div className='md:order-1'>
+     <Link className='' to='/'>
+       <img className='h-32 mb-5' src='/images/aklogo.png' alt='Logo' />
+     </Link>
+   </div>
+ </nav>
+ 
   );
 }
 
 export default Navbar;
+
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,39 @@
-import React from 'react'
-import { Link } from "react-router-dom"
+import React from 'react';
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <div className='home'>
-      <div className='info'>
-         <h1 className='myname'> I am Alejandra Kennedy</h1>
-         <p className='myselfp'>I am a web developer with an Industrial Engineering background, proficient in JavaScript, CSS, HTML, and React. My experience as a teacher has taugth me the importance of collaboration, communication, and adaptability. My goal is to leverage my skills to be a hardworking asset to any team, while also continuing to grow as a web developer</p>
+   <div className="home bg-aliceblue p-4 md:p-10 flex flex-col md:flex-row md:items-center md:justify-center">
+      <div className="info flex flex-col md:p-10 md:justify-center ">
+         <h1 className="myname text-4xl font-bold leading-tight md:leading-normal">I am Alejandra Kennedy</h1>
+         <p className="myselfp text-2xl leading-normal mt-4 md:mt-6">I am a web developer with an Industrial Engineering background, proficient in JavaScript, CSS, HTML, and React. My experience as a teacher has taught me the importance of collaboration, communication, and adaptability. My goal is to leverage my skills to be a hardworking asset to any team, while also continuing to grow as a web developer</p>
+
+         <div className="social flex flex-row p-3 m-1 md:p-2 ">
+         <a href="https://github.com/alehacker" className="w-10 h-10 object-contain object-center mb-2 md:mb-0 md:mr-2">
+            <img src='/images/githublogo.png' alt='github'/>
+         </a>
+         <a href="https://twitter.com/AleInTech" className="w-10 h-10 object-contain object-center mb-2 md:mb-0 md:mr-2">
+            <img src='/images/twitterlogo.png' alt='twitter'/>
+         </a>
+         <a href="https://www.linkedin.com/in/alejandra-kennedy/" className="w-10 h-10 object-contain object-center md:mb-0">
+            <img src='/images/linkediicon.png' alt='linkedin'/>
+         </a>
       </div>
-    </div>
+      
+  </div>
+ 
+
+
+  <div className="akphoto flex flex-col  md:p-2">
+      <img src='/images/akprofilepic.png' alt='AKPhoto' className="w-full max-w-full md:max-w-none md:w-48 mx-auto"/>
+      <div className='flex flex-row  gap-1 p-1 m-1 justify-center items-center md:p-2 ' >
+         <img className= 'w-10 h-10 md:w-5 md:h-5'src='/images/emailicon.png' alt='email'/>
+         <p className='text-xl p-1'>Reach Out</p>
+      </div>
+  </div>
+</div>
+
   )
 }
 
-export default Home
+export default Home;
